fb9481e81ff50dc7aae206f1d9a3254b21a1d02f
clonq <ovi.comes@gmail.com>
2015-06-29 22:09:31 -0400

    user:login.response handler

--- component.js
+++ component.js
@@ -1,10 +1,32 @@
 module.exports = function(){
     var _ = require('underscore');
+    var dao = require('daoi');
     this.init = function(config) {
         var self = this;
+        var daoImpl = dao.use(dao.FILE);
+        dao.register('profile');
         process.emit('http.route:create', { path:'/profile/:id', trigger:'profile:view', webpage:'/'});
-        process.on('profile:view', function(pin){
-            console.log('profile:view', pin)
+        process.on('user:login.response', function(pin){
+            var pout = {};
+            if (pin && pin.user && pin.user.$id) {
+                var criteria = { user$id: pin.user.$id };
+                daoImpl
+                .profile
+                .findOne(criteria)
+                .then(function(profile){
+                    if(profile) {
+                        pout = { profile: profile };
+                        process.emit('profile:get.response', pout);
+                    } else {
+                        pout = { for: criteria };
+                        process.emit('profile:not.found', pout);
+                    }
+                })
+                .catch(function(err){
+                    //todo
+                    console.log(err)
+                });
+            }
         });
     }
 }
